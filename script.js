03f66139e027c33b033f873b741335d77d0b0d47
Yehui Huang <yehui.usyd@gmail.com>
2024-02-19 17:13:03 +1100

    creat script

---
+++ script.js
@@ -0,0 +1,15 @@
+// List data
+const data = {
+  fruits: [
+    'Apple',
+    'Banana',
+    'Pear',
+    'Watermelon',
+    'Grape',
+    'Strawberry',
+    'Mango',
+    'Blackberry',
+  ],
+  spices: ['Salt', 'Pepper', 'Chilli', 'Herbs', 'Curry'],
+  vegetables: ['Carrot', 'Cucumber'],
+};
